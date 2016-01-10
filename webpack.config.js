56f37285e60eab0f59d8d2f749b37381114ff9dd
Bastian Schade <bastianschadeberlin@googlemail.com>
2016-01-10 19:47:42 +0100

    Add webpack config

---
+++ webpack.config.js
@@ -0,0 +1,24 @@
+var path = require('path')
+
+module.exports = {
+
+  module: {
+    loaders: [
+      {
+        test: /\.jsx?$/,
+        exclude: /(node_modules|bower_components)/,
+        loader: 'babel-loader',
+        query: {
+          presets: ['react', 'es2015']
+        }
+      }
+    ]
+  },
+
+  entry: './src/main.jsx',
+
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'main.js'
+  }
+}
