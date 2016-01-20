744be9ed2c7ef37ece27694b0360879c79b57871
Bastian Schade <bastianschadeberlin@googlemail.com>
2016-01-20 23:35:18 +0100

    Dispatch action to select item

--- src/containers/main.jsx
+++ src/containers/main.jsx
@@ -1,13 +1,19 @@
 import React, { Component } from 'react'
 import Gallery from '../components/gallery'
 import { connect } from 'react-redux'
+import { selectItem } from '../actions/'
 
 class Main extends Component {
+
+  handleClick = (idx) => {
+    this.props.dispatch(selectItem(idx))
+  };
+
   render() {
     const { images } = this.props
     return (
       <div>
-        <Gallery activeIdx="0" images={images} />
+        <Gallery handleClick={this.handleClick} activeIdx={images.selectedItem} images={images.items} />
       </div>
     )
   }
