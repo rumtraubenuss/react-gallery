0a7438c620a8062a6972277e0eb7b512174f36c8
Bastian Schade <bastianschadeberlin@googlemail.com>
2016-01-23 00:58:12 +0100

    Implement router

--- src/index.jsx
+++ src/index.jsx
@@ -2,25 +2,41 @@ import 'whatwg-fetch'
 import 'babel-polyfill'
 import React from 'react'
 import ReactDOM from 'react-dom'
+import App from './containers/app'
 import Main from './containers/main'
 import Blank from './components/blank'
 import rootReducer from './reducers'
 import sagaMiddleware from 'redux-saga'
-import { createStore, applyMiddleware } from 'redux'
+import { createStore, applyMiddleware, combineReducers } from 'redux'
 import { Provider } from 'react-redux'
 import { loadImages, receiveImages } from './actions'
 import rootSaga from './sagas'
+import { Router, Route, browserHistory, IndexRoute} from 'react-router'
+import { syncHistory, routeReducer } from 'redux-simple-router'
+
+const reducer = combineReducers(Object.assign({}, rootReducer, {
+  routing: routeReducer
+}))
+
+const reduxRouterMiddleware = syncHistory(browserHistory)
 
 const createStoreWithMiddleware = applyMiddleware(
-  sagaMiddleware(rootSaga)
+  sagaMiddleware(rootSaga),
+  reduxRouterMiddleware
 )(createStore)
+
 const store = createStoreWithMiddleware(rootReducer)
 
 store.dispatch(loadImages())
 
 ReactDOM.render(
   <Provider store={store}>
-    <Blank />
+    <Router history={browserHistory}>
+      <Route path="/" component={App}>
+        <IndexRoute path="foo" component={Main} />
+        <Route path="blank" component={Blank} />
+      </Route>
+    </Router>
   </Provider>,
   document.getElementById('app')
 )
