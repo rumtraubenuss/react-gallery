544fc6e5150775cac0adef4d0fa2abe4ffae1df0
Bastian Schade <bastianschadeberlin@googlemail.com>
2016-01-22 23:28:27 +0100

    Add blank component

--- src/index.jsx
+++ src/index.jsx
@@ -3,6 +3,7 @@ import 'babel-polyfill'
 import React from 'react'
 import ReactDOM from 'react-dom'
 import Main from './containers/main'
+import Blank from './components/blank'
 import rootReducer from './reducers'
 import sagaMiddleware from 'redux-saga'
 import { createStore, applyMiddleware } from 'redux'
@@ -19,7 +20,7 @@ store.dispatch(loadImages())
 
 ReactDOM.render(
   <Provider store={store}>
-    <Main />
+    <Blank />
   </Provider>,
   document.getElementById('app')
 )
