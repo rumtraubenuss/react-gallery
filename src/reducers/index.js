4fffe02d9ff5ec056bc208a9bf38202bd2bf0819
Bastian Schade <bastianschadeberlin@googlemail.com>
2016-01-24 09:00:47 +0100

    Return current paginated items when page not valid
    TODO: Return valid page

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -9,33 +9,45 @@ const initialImageState = {
   itemsPerPage: 2
 }
 
-function paginate(items, page, itemsPerPage=2) {
-  return items.slice((page - 1) * itemsPerPage, page * itemsPerPage)
+function paginate(items, page, itemsPerPage=2, itemsPaginatedCurrent) {
+  if(page < 1 || !(Math.floor(page * itemsPerPage) <= items.length)) {
+    return {itemsPaginatedCurrent, page}
+  } else {
+    return {
+      items: items.slice((page - 1) * itemsPerPage, page * itemsPerPage),
+      page
+    }
+  }
 }
 
 function images(state = initialImageState, action) {
+  let res
   switch(action.type) {
     case constants.RECEIVE_IMAGES:
+      res = paginate(action.images, state.pageCurrent, state.itemsPerPage, state.itemsPaginated)
       return Object.assign({}, state,
         {
           items: action.images,
-          itemsPaginated: paginate(action.images, state.pageCurrent, state.itemsPerPage)
+          itemsPaginated: res.items,
+          pageCurrent: res.page
         }
       )
     case constants.SELECT_ITEM:
       return Object.assign({}, state, {selectedItem: action.id})
     case constants.PAGINATE_IMAGES_NEXT:
+      res = paginate(state.items, state.pageCurrent + 1, state.itemsPerPage, state.itemsPaginated)
       return Object.assign({}, state,
         {
-          itemsPaginated: paginate(state.items, state.pageCurrent + 1, state.itemsPerPage),
-          pageCurrent: state.pageCurrent + 1
+          itemsPaginated: res.items,
+          pageCurrent: res.page
         }
       )
     case constants.PAGINATE_IMAGES_PREV:
+      res = paginate(state.items, state.pageCurrent - 1, state.itemsPerPage, state.itemsPaginated)
       return Object.assign({}, state,
         {
-          itemsPaginated: paginate(state.items, state.pageCurrent - 1, state.itemsPerPage),
-          pageCurrent: state.pageCurrent - 1
+          itemsPaginated: res.items,
+          pageCurrent: res.page
         }
       )
   }
