e793087da64c1f5825cf1285b2d62ee4394949d6
Bastian Schade <bastianschadeberlin@googlemail.com>
2016-01-29 23:53:39 +0100

    Add faucet

--- test/test.js
+++ test/test.js
@@ -2,6 +2,6 @@ import test from 'tape'
 
 test('foo bar', t => {
   t.plan(2)
-  t.equal(typeof Date.now, 'function')
-  t.equal(typeof Math, 'object')
+  t.equal(typeof Date.now, 'object', 'Date.now should be a function')
+  t.equal(typeof Math, 'function', 'Math should be an object')
 })
