3bbd8a1de94db992a17db42c60f9e185109ff7cf
Bastian Schade <bastianschadeberlin@googlemail.com>
2016-01-30 00:04:04 +0100

    Add besic tests for paginate

--- test/test.js
+++ test/test.js
@@ -1,7 +1,9 @@
 import test from 'tape'
+import { NEXT, PREV, paginate } from '../src/utils/'
 
-test('foo bar', t => {
-  t.plan(2)
-  t.equal(typeof Date.now, 'object', 'Date.now should be a function')
-  t.equal(typeof Math, 'function', 'Math should be an object')
+test('utils', t => {
+  t.notEqual(typeof NEXT, 'undefined', 'NEXT should not be undefined')
+  t.notEqual(typeof PREV, 'undefined', 'PREV should not be undefined')
+  t.equal(typeof paginate, 'function', 'paginate should be a function')
+  t.end()
 })
