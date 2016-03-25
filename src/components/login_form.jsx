a6fa6127725a13b8b20d391d70af294743263172
Bastian Schade <bastianschadeberlin@googlemail.com>
2016-03-25 01:35:32 +0100

    Add basic form validation

--- src/components/login_form.jsx
+++ src/components/login_form.jsx
@@ -1,15 +1,28 @@
 import React, {Component} from 'react'
 import {reduxForm} from 'redux-form'
 
+const validate = values => {
+  const errors = {}
+  if(!values.email) {
+    errors.email = 'Required'
+  }
+  if(!values.password) {
+    errors.password = 'Required'
+  }
+  return errors
+}
+
 let LoginForm = (props) => {
     const {fields: {password, email}, handleSubmit} = props
     return (
       <form onSubmit={handleSubmit}>
         <div>
           <input type="email" placeholder="Email" {...email}/>
+          {email.touched && email.error && <div>{email.error}</div>}
         </div>
         <div>
           <input type="password" placeholder="Password" {...password}/>
+          {password.touched && password.error && <div>{password.error}</div>}
         </div>
         <button type="submit">Submit</button>
       </form>
@@ -18,7 +31,8 @@ let LoginForm = (props) => {
 
 LoginForm = reduxForm({
   form: 'login',
-  fields: ['email', 'password']
+  fields: ['email', 'password'],
+  validate,
 })(LoginForm)
 
 export default LoginForm
