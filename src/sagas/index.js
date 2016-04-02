56481793b9a0aeba8cf6d138003eb781fd532aa7
Bastian Schade <bastianschadeberlin@googlemail.com>
2016-04-02 02:12:11 +0200

    Add saga to trigger firebas login
    Still logging to console for debugging.

--- src/sagas/index.js
+++ src/sagas/index.js
@@ -2,6 +2,9 @@ import { take, put, call, fork, race } from 'redux-saga'
 import { receiveImages, endDummyTimeoutRedirect } from '../actions'
 import * as constants from '../constants/'
 import { routeActions } from 'redux-simple-router'
+import Firebase from 'firebase'
+
+const firebase = new Firebase('https://popping-fire-3816.firebaseio.com')
 
 export function* fetchImages() {
   while(yield take(constants.LOAD_IMAGES)) {
@@ -19,9 +22,19 @@ export function* dummyTimeoutRedirect() {
   }
 }
 
+export function* firebaseLogin(action) {
+  while(true) {
+    const { email, password, type } = yield take(constants.TRIGGER_LOGIN)
+    console.log(firebase.authWithPassword({email, password}))
+    //const res = yield call(firebase.authWithPassword, {email: email, password: password})
+    //console.log('SAGA LOGIN TRIGGERED', email, password, type)
+  }
+}
+
 export default function* root() {
   yield fork(fetchImages)
   yield fork(dummyTimeoutRedirect)
+  yield fork(firebaseLogin)
 }
 
 function delay(time) {
