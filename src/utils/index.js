673fd408e0c6ae3aebdc4d35b00e1e5c04dec4bd
Bastian Schade <bastianschadeberlin@googlemail.com>
2016-04-13 23:01:50 +0200

    Refactor following eslint rules

--- src/utils/index.js
+++ src/utils/index.js
@@ -1,25 +1,26 @@
-export const NEXT = 'NEXT'
-export const PREV = 'PREV'
-export function paginate (items, pageCurrent, itemsPerPage, itemsPaginatedCurrent, direction=undefined) {
-  if(typeof direction != 'undefined') {
-    const cond_a = pageCurrent <= 1 && direction === PREV
-    const cond_b = pageCurrent * itemsPerPage >= items.length && direction === NEXT
-    if(cond_a || cond_b) {
-      return(
+export const NEXT = 'NEXT';
+export const PREV = 'PREV';
+export function paginate(items, pageCurrent, itemsPerPage,
+                         itemsPaginatedCurrent, direction = undefined) {
+  if (typeof direction !== 'undefined') {
+    const condA = pageCurrent <= 1 && direction === PREV;
+    const condB = pageCurrent * itemsPerPage >= items.length && direction === NEXT;
+    if (condA || condB) {
+      return (
         {
           items: itemsPaginatedCurrent,
-          page: pageCurrent
+          page: pageCurrent,
         }
-      )
+      );
     }
   }
-  let newPage = pageCurrent
-  if(direction === NEXT) newPage += 1
-  if(direction === PREV) newPage -= 1
+  let newPage = pageCurrent;
+  if (direction === NEXT) newPage += 1;
+  if (direction === PREV) newPage -= 1;
   return (
     {
       items: items.slice((newPage - 1) * itemsPerPage, newPage * itemsPerPage),
-      page: newPage
+      page: newPage,
     }
-  )
+  );
 }
