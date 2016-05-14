6b1213682c18d7fb07f13056e9e12650df204144
Bastian Schade <bastianschadeberlin@googlemail.com>
2016-05-14 06:51:30 +0200

    Fix eslint errors

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,42 +1,42 @@
-import * as constants from '../constants/'
-import { NEXT } from '../utils/'
+import * as constants from '../constants/';
+import { NEXT } from '../utils/';
 
 export function receiveImages(images) {
   return {
     type: constants.RECEIVE_IMAGES,
-    images: images
-  }
+    images,
+  };
 }
 
 export function loadImages() {
   return {
-    type: constants.LOAD_IMAGES
-  }
+    type: constants.LOAD_IMAGES,
+  };
 }
 
 export function triggerDummyTimeoutRedirect() {
   return {
-    type: constants.TRIGGER_DUMMY_TIMEOUT_REDIRECT
-  }
+    type: constants.TRIGGER_DUMMY_TIMEOUT_REDIRECT,
+  };
 }
 
 export function endDummyTimeoutRedirect() {
   return {
-    type: constants.END_DUMMY_TIMEOUT_REDIRECT
-  }
+    type: constants.END_DUMMY_TIMEOUT_REDIRECT,
+  };
 }
 
 export function paginateImages(direction) {
   return {
-    type: direction === NEXT ? constants.PAGINATE_IMAGES_NEXT : constants.PAGINATE_IMAGES_PREV
-  }
+    type: direction === NEXT ? constants.PAGINATE_IMAGES_NEXT : constants.PAGINATE_IMAGES_PREV,
+  };
 }
 
 export function selectItem(id) {
   return {
     type: constants.SELECT_ITEM,
-    id: id
-  }
+    id,
+  };
 }
 
 export function triggerLogin(email, password) {
@@ -44,20 +44,20 @@ export function triggerLogin(email, password) {
     type: constants.TRIGGER_LOGIN,
     email,
     password,
-  }
+  };
 }
 
 export function triggerLogout() {
   return {
     type: constants.TRIGGER_LOGOUT,
-  }
+  };
 }
 
 export function authChange(uid) {
   return {
     type: constants.AUTH_CHANGE,
-    uid
-  }
+    uid,
+  };
 }
 
 export function pushNode(node, val) {
@@ -65,14 +65,14 @@ export function pushNode(node, val) {
     type: constants.PUSH_NODE,
     node,
     val,
-  }
+  };
 }
 
 export function networkChange(status, transactionObject = {}) {
-  const type = status == 'start' ? constants.NETWORK_SYNC_START : constants.NETWORK_SYNC_STOP
+  const type = status === 'start' ? constants.NETWORK_SYNC_START : constants.NETWORK_SYNC_STOP;
   return {
     type,
-    transactionObject
-  }
+    transactionObject,
+  };
 }
 
