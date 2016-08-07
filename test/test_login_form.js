f34bc8c43ef77a6bc23eca9c84fc348fad420384
Bastian Schade <bastianschadeberlin@googlemail.com>
2016-08-07 22:29:16 +0200

    Add sinon and add test for handleSubmit

--- test/test_login_form.js
+++ test/test_login_form.js
@@ -2,9 +2,10 @@ import React from 'react';
 import test from 'tape';
 import { LoginForm } from '../src/components/login_form';
 import { shallow } from 'enzyme';
+import sinon from 'sinon';
 
 test('Login form render output', t => {
-  const component = shallow(<LoginForm  fields={{ password: {}, email: {} }}/>);
+  const component = shallow(<LoginForm fields={{ password: {}, email: {} }}/>);
 
   const expected = {
     input: 2,
@@ -16,5 +17,22 @@ test('Login form render output', t => {
   }
 
   t.deepEqual(expected, actual, 'It should render 2 input fields and 1 submit button');
+  t.end();
+});
+
+test('Login form on submit', t => {
+  const handleSubmitSpy = sinon.spy();
+  const component = shallow(
+    <LoginForm handleSubmit={handleSubmitSpy}
+               fields={{ password: {}, email: {} }}
+    />
+  );
+
+  component.find('form').simulate('submit');
+
+  const expected = 1;
+  const actual = handleSubmitSpy.callCount;
+
+  t.equal(expected, actual, 'It should call handleSubmit on submit');
   t.end();
 });
